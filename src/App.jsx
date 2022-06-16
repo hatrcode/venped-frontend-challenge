402b2deb423c1fa77617f873b9ce1d551d3654ee
Ha Truong <td.xuha@gmail.com>
2022-06-16 16:24:12 +0700

    fix: update PageNotFound path

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-import './css/style.scss';
+import "./css/style.scss";
 
 // Import pages
-import Products from './pages/Products';
-import PageNotFound from './pages/utility/PageNotFound';
+import Products from "./pages/Products";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
   }, [location.pathname]); // triggered on route change
 
   return (
