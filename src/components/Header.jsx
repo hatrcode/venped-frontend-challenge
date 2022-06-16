358f7f5059ba098e46a8681f0e788fb06bec9455
Ha Truong <td.xuha@gmail.com>
2022-06-16 16:28:56 +0700

    feat: add LangBar to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,18 @@
-import React from 'react';
+import React from "react";
+import LangBar from "./LangBar";
 
 function Header() {
-
-
   return (
     <header className="sticky top-0 bg-white border-b border-slate-200 z-30">
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 -mb-px">
-
           {/* Header: Left side */}
-          <div className="flex">
-          </div>
+          <div className="flex"></div>
 
           {/* Header: Right side */}
           <div className="flex items-center space-x-3">
-            {/* Suggested space for translation flags */}...
-
+            <LangBar />
           </div>
-
         </div>
       </div>
     </header>
